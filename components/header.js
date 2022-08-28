231188d789920a7e98717543643fe413dead8dbb
scripty-script <scriptyscript2022@gmail.com>
2022-08-28 21:12:20 +0800

    Add mobile nav menu

--- components/header.js
+++ components/header.js
@@ -1,8 +1,10 @@
 import Link from 'next/link'
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react'
 
 export default function Header() {
     const [y, setY] = useState(0);
+    const [open, setOpen] = useState(false)
 
     useEffect(() => {
         document.onscroll = (e) => {
@@ -15,23 +17,23 @@ export default function Header() {
             <div className='container flex justify-between items-center py-4'>
                 <h1 className='text-2xl font-bold'>AnimeBinge</h1>
                 <nav className='hidden md:block'>
-                    <ul className='flex space-x-4'>
-                        <li>
+                    <ul className='flex items-center space-x-4'>
+                        <li className='px-5 p-2'>
                             <Link href={"#home"}>
-                                <a className='px-5 p-2'>Home</a>
+                                <a>Home</a>
                             </Link>
                         </li>
-                        <li>
+                        <li className='px-5 p-2'>
                             <Link href={"#discover"}>
-                                <a className='px-5 p-2'>Discover</a>
+                                <a>Discover</a>
                             </Link>
                         </li >
-                        <li>
+                        <li className='px-5 p-2'>
                             <Link href={"#about-us"}>
-                                <a className='px-5 p-2'>About us</a>
+                                <a>About us</a>
                             </Link>
                         </li>
-                        <li >
+                        <li>
                             <Link href={'#signup'}>
                                 <a className='px-5 p-2 rounded-md border border-white bg-transparent'>Sign Up</a>
                             </Link>
@@ -43,7 +45,63 @@ export default function Header() {
                         </li>
                     </ul>
                 </nav>
+                <button className='md:hidden' onClick={() => { setOpen(!open) }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </button>
+                <Sidebar open={open} setOpen={setOpen} />
             </div>
         </header>
     )
 }
+
+const Sidebar = ({ open, setOpen }) => (
+    <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={() => { }}>
+            <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <div className="fixed z-10 inset-0 top-16 overflow-y-auto">
+                    <Dialog.Panel className="absolute inset-0 bg-white">
+                        <div className='mx-auto w-72 pt-10'>
+                            <ul className='flex flex-col divide-y'>
+                                <li className='px-5 p-2'>
+                                    <Link href={"#home"}>
+                                        <a>Home</a>
+                                    </Link>
+                                </li>
+                                <li className='px-5 p-2'>
+                                    <Link href={"#discover"}>
+                                        <a  >Discover</a>
+                                    </Link>
+                                </li >
+                                <li className='px-5 p-2'>
+                                    <Link href={"#about-us"}>
+                                        <a>About us</a>
+                                    </Link>
+                                </li>
+                                <li className='px-5 p-2'>
+                                    <Link href={'#signup'}>
+                                        <a>Sign Up</a>
+                                    </Link>
+                                </li>
+                                <li className='px-5 p-2'>
+                                    <Link href={'#login'}>
+                                        <a>Log In</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </Dialog.Panel>
+                </div>
+            </Transition.Child>
+        </Dialog>
+    </Transition.Root>
+)
